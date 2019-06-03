295c552e4ed6ccd553abb38521c5c8a4a4483965
Mathieu RACASSIN <mam.ath@hotmail.fr>
2019-06-03 22:04:01 +0200

    add Fizz and Buzz case

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        private int input;
+
+        public FizzBuzz(int input)
+        {
+            this.input = input;
+        }
+
+        public string Compute()
+        {
+            if (this.input % 3 == 0)
+                return "Fizz";
+            if (this.input % 5 == 0)
+                return "Buzz";
+            return this.input.ToString();
+        }
+    }
+}
--- FizzBuzzTest/UnitTest1.cs
+++ FizzBuzzTest/UnitTest1.cs
@@ -1,14 +1,30 @@
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
 
 namespace FizzBuzzTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataRow(1, "1")]
+        [DataRow(2, "2")]
+        [DataRow(3, "Fizz")]
+        [DataRow(4, "4")]
+        [DataRow(5, "Buzz")]
+        [DataRow(6, "Fizz")]
+        [DataRow(7, "7")]
+        [DataRow(8, "8")]
+        [DataRow(9, "Fizz")]
+        [DataRow(10, "Buzz")]
+        [DataRow(11, "11")]
+        [DataRow(12, "Fizz")]
+        [DataTestMethod]
+        public void TestMethod1(int input, string result)
         {
-
+            FizzBuzz.FizzBuzz fizzBuzz = new FizzBuzz.FizzBuzz(input);
+            string resultMethod = fizzBuzz.Compute();
+            resultMethod.Should().Be(result);
         }
     }
 }
